e5ca073020c974c1265e2dfa9157002d64726c2b
David <72582527+David2271@users.noreply.github.com>
2024-09-26 13:14:37 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit_ui
+
+def main():
+    ui = streamlit_ui.ui()
+
+
+if __name__ == '__main__':
+    main()
---
+++ streamlit_ui.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+class ui():
+    def __init__(self) -> None:
+        st.title('Baseball App')
